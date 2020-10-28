8630480388176dea5b33f01876ece464aa89279e
rubysattar <rubysattar@gmail.com>
2020-10-28 14:43:49 -0400

    initial commit

---
+++ arraypractice/Array.java
@@ -0,0 +1,23 @@
+// import jdk.javadoc.internal.doclets.formats.html.resources.standard;
+
+class Array {
+    public static void main ( String[] args ) {
+
+        // declaring a string array with a limit of 3
+        // when this string was declared, it was assigned 3 values
+        // you can't assign anymore to this array after declaration
+        String[] myStringArray = { "Java", "Is", "Specific" };
+
+        // declaring a new integer array with a limit of 3
+        // when this array was declared, it was also assigned a max of 3 values
+        // which we define later outside of the declaration
+        int[] myNumberArray = new int[3];
+        myNumberArray[0] = 50;
+        myNumberArray[1] = 60;
+        myNumberArray[2] = 70;
+
+        System.out.println( "String array length is " + myStringArray.length);
+        System.out.println( "Integer array length is " + myNumberArray.length);
+        System.out.println( myStringArray[0] + " " + myStringArray[1] + " " + myStringArray[2]);
+    }
+}
